4f166bf91850bf2fb49a571067a8b305060f0dc9
khaleel pathan <pathankhaleel8@gmial.com>
2021-04-12 12:57:32 +0530

    First commit

---
+++ src/com/nt/git/GitProject.java
@@ -0,0 +1,18 @@
+package com.nt.git;
+
+public class GitProject {
+
+	public static void main(String[] args) {
+
+		// Using Scanner for Getting Input from User
+
+		String hungry = "yes";
+
+		if (hungry == "yes") {
+			System.out.println("eat something...");
+		} else
+			System.out.println("Do work....");
+
+	}
+
+}
